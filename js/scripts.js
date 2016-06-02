b49db6747f885c14055f1886992dee31101d2024
Benjamin Heflin and Julia Dickey <julia@>
2016-06-02 14:54:39 -0700

    Create third question

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,60 @@ $(document).ready (function(){
   $("select#favTopic").change(function(event) {
     var topic = parseInt($("#favTopic").val());
     event.preventDefault();
-    $("#followUp1").hide();
-    $("#followUp2").hide();
-    $("#followUp3").hide();
+    $("#followUp2a").hide();
+    $("#followUp2b").hide();
+    $("#followUp2c").hide();
 
     if (topic === 1 ) {
-      $("#followUp1").show();
+      $("#followUp2a").show();
     }
     else if (topic === 2 ) {
-      $("#followUp2").show();
+      $("#followUp2b").show();
     }
     else  {
-      $("#followUp3").show();
+      $("#followUp2c").show();
     }
   });
+
+  $("select#favArt").change(function(event) {
+    var artist = parseInt($("#favArt").val());
+    event.preventDefault();
+    // $("#followUp3").hide();
+
+    if (artist >= 0 ) {
+      $("#followUp3").show();
+    }
+    else  {
+      ("#followUp3").show();
+    }
+  });
+
+  $("select#favTech").change(function(event) {
+    var tech = parseInt($("#favTech").val());
+    event.preventDefault();
+    // $("#followUp3").hide();
+
+    if (tech >= 0 ) {
+      $("#followUp3").show();
+    }
+    else  {
+      ("#followUp3").show();
+    }
+  });
+
+  $("select#favFit").change(function(event) {
+    var fit = parseInt($("#favFit").val());
+    event.preventDefault();
+    // $("#followUp3").hide();
+
+    if (fit >= 0 ) {
+      $("#followUp3").show();
+    }
+    else  {
+      ("#followUp3").show();
+    }
+  });
+
+
+
 });
